582ee2e5d9558a8dbe94d9c692742129f4e9d26d
reuel-ly <riweriweri@gmail.com>
2026-03-09 10:35:51 +0800

    chore: load environment variables of APIs

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,16 @@
+# Configuration settings for the Weather Data Pipeline
+import os
+from dotenv import load_dotenv
+load_dotenv()
+OPENWEATHER_KEY = os.getenv("OPENWEATHER_KEY")
+OPENWEATHER_URL = os.getenv("OPENWEATHER_URL")
+
 # Kafka
 KAFKA_BROKER = "localhost:29092"
 KAFKA_TOPIC = "weather"
 
 # Spark
 SPARK_APP_NAME = "WeatherPipeline"
+
+#MongoDB
+MONGO_URI = "mongodb://localhost:27017/weather_db"
