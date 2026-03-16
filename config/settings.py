1012146019a478e72878d208046ed6b9e385bbd9
Reuel Pornobe <riweriweri@gmail.com>
2026-03-16 14:13:52 +0800

    chore(config): new MQTT Broker, MQTT Port, Spark Topic, Kafka Topic, and MQTT Topic

--- config/settings.py
+++ config/settings.py
@@ -8,10 +8,17 @@ OPENWEATHER_URL = os.getenv("OPENWEATHER_URL")
 # Kafka
 KAFKA_BROKER = "localhost:29092"
 KAFKA_TOPIC = "weather"
+KAFKA_TOPIC_IOT = "iot-sensors"  # separate topic from weather
 
 # Spark
 SPARK_APP_NAME = "WeatherPipeline"
+SPARK_APP_NAME_IOT = "WastePipeline"
 
 #MongoDB
 MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB = os.getenv("MONGO_DB")
+
+# MQTT
+MQTT_BROKER = "localhost"
+MQTT_PORT = 1883
+MQTT_TOPIC = "iot/sensors"
